72d58574ff9d4440089630d917a2b28d916332a4
1101137212 <a4274316@gmail.com>
2015-08-28 20:45:44 +0800

    後台架構完成

---
+++ MODERN-ART-ASSOCIATION-OF-KAOHSIUNG/Controllers/BEX/BEXController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MODERN_ART_ASSOCIATION_OF_KAOHSIUNG.Controllers.BEX
+{
+    public class BEXController : Controller
+    {
+        // GET: BEX
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MODERN-ART-ASSOCIATION-OF-KAOHSIUNG/Controllers/BLN/BLNController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MODERN_ART_ASSOCIATION_OF_KAOHSIUNG.Controllers.BLN
+{
+    public class BLNController : Controller
+    {
+        // GET: BLN
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
